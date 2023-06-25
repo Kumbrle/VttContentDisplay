6054eac6d081059cc1856803233f6cd52af16763
Kumbrle <86300730+Kumbrle@users.noreply.github.com>
2023-06-25 13:45:23 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+import requests
+
+app = Flask(__name__) 
+
+@app.route("/", methods = ["Get"]) 
+def home():
+  url = request.args.get("vtt")
+  r = requests.get(url, allow_redirects=True)
+  r.headers.add("Access-Control-Allow-Origin", "*")
+  return r.content
+
+if __name__ == "__main__":
+  app.run() 
